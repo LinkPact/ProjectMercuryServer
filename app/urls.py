df9bb050e26a48e09b41ba6f62a30af91b945e36
Jakob Willforss <jakob.willforss@hotmail.com>
2017-07-26 20:10:59 +0200

    Basic API setup  working

--- app/urls.py
+++ app/urls.py
@@ -2,17 +2,11 @@ from django.conf.urls import url, include
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'is_staff')
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+from app import views
 
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 
 urlpatterns = [
